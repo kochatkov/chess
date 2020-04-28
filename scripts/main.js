90eb4fc2a6f78b569506f6d58e03b37913bd51e5
kochatkov <vlad.kochatkov@gmail.com>
2020-04-28 17:12:29 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,31 @@
 'use strict';
 
-const section = document.querySelector('.chess');
+const WHITE_CELL_COLOR = 'chess__white';
+const BLACK_CELL_COLOR = 'chess__black';
+const CHESS_FIELD = 'chess__field';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const section = document.querySelector('.chess');
   const div = document.createElement('div');
+  let cells;
 
   section.append(div);
-  div.className = 'chess__field';
 
-  const colors = ['chess__white', 'chess__black'];
+  div.className = CHESS_FIELD;
 
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-      const cells = document.createElement('div');
-
-      if (i % 2 === j % 2) {
-        cells.className = colors[0];
-      } else {
-        cells.className = colors[1];
-      }
+      cells = document.createElement('div');
+      changeCellColor(cells, i, j);
       div.append(cells);
     }
   }
 });
+
+function changeCellColor(cells, i, j) {
+  if (i % 2 === j % 2) {
+    cells.className = WHITE_CELL_COLOR;
+  } else {
+    cells.className = BLACK_CELL_COLOR;
+  }
+}
